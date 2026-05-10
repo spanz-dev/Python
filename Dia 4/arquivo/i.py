1c12b4a4b95ba21287111ea9feae9bb879b90432
sandrr15 <joaovitorenzopsn@gmail.com>
2026-05-10 01:28:23 -0300

    Delete Dia 4 directory

--- Dia 4/arquivo/i.py
+++
@@ -1,5 +0,0 @@
-arquivo = open("dados.txt", "r")
-linhas = arquivo.readlines()
-for l in linhas:
-    print(linhas.strip())
-arquivo.close()
--- Dia 4/arquivo/main2.py
+++
@@ -1,7 +0,0 @@
-def contar_linhas(cadastro):
-    linha = 1
-    with open(cadastro, "r", encoding="utf-8") as arquivo:
-        return sum(1 for linha in arquivo)
-    
-quantidade = contar_linhas('cadastro.txt')
-print(f"O arquivo possui {quantidade} linhas.")
